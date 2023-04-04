e87bedcc1bd2bdef0e60f0d2072b9ce541fa0bfe
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-04-04 10:51:23 +0400

    data with promise

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import Card from "./components/Card";
+import { IProduct } from "./model";
+import { useEffect, useState } from "react";
+
+
+
+
+const URL = `https://fakestoreapi.com/products`
+
+
+
+
+
 
 function App() {
+ 
+  const [data, setData] = useState<IProduct[]>([])
+  const [dataIsReady, setDataIsReady] = useState(false)
+
+  const getData = async () => {
+
+    const products = await axios(URL)
+    .then(res=>res.data)
+  setDataIsReady(true)
+    setData(products)
+   
+  }
+
+  const getDataWithPromise = ()=> {
+    return new Promise((resolve, reject)=>{
+      resolve(
+        axios(URL)
+    .then(res=>res.data)
+    .then(res=>{
+
+      setData(res)
+      return res
+    })
+      )
+    })
+  }
+
+useEffect(()=>{
+  getDataWithPromise()
+  .then(res=>{
+    return res
+  })
+  .then(res=>{
+    setDataIsReady(true)
+    console.log(res)
+    return res
+  })
+},[])
+
+  // useEffect(()=>{
+  //   getData()
+    
+  //  console.log(data)
+  // }, [dataIsReady])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+   <div className="w-4/5 m-auto flex flex-col items-center  content-center ">
+   {
+      dataIsReady? 
+      data.map(item=>(
+        <Card key={item.id} data = {item}/>
+      )): <div>Loading...</div>
+    }
+   </div>
+
     </div>
   );
 }
